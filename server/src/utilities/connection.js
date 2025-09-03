17f21f0f89e3de16026e7ebd2de495056ced90dd
Varaprasad Akireddy <akireddyvaraprasad58@gmail.com>
2025-09-03 23:35:53 +0530

    first_commit

---
+++ server/src/utilities/connection.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const url = process.env.URL;
+
+const registrationSchema = new mongoose.Schema({
+  name: { type: String, required: [true, "Required field"] },
+  branch: { type: String, required: [true, "Required field"] },
+  year: { type: Number, required: [true, "Required field"] },
+  mobile: { type: Number, required: [true, "Required field"] },
+  mail: { type: String, default: "No mail id" },
+  amount: { type: Number, required: [true, "Required field"] },
+  paymentMode: { type: String, required: [true, "Required field"] },
+  date: {type: Date,default: Date.now()},
+}, { collection: "IsteMembers" });
+
+let connection = {};
+
+connection.getRegistrationCollection = async () => {
+  try {
+    return (await mongoose.connect(url)).model("IsteMembers",registrationSchema);
+  } catch (err) {
+    let error = new Error("Could not connect to the database");
+    error.status = 500;
+    throw error;
+  }
+};
+
+module.exports = connection;
